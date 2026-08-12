29a4adcfa9c84c103e3294b478d4434a4ca442cc
1SosukeAizen1 <aizengoku1@gmail.com>
2026-08-12 17:33:21 -0500

    test

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -35,7 +35,7 @@ export default function HomeScreen() {
         <ThemedView style={styles.heroSection}>
           <AnimatedIcon />
           <ThemedText type="title" style={styles.title}>
-            Hello World
+             Welcome to Atlas Lakes here we will be having our apps
           </ThemedText>
         </ThemedView>
 
